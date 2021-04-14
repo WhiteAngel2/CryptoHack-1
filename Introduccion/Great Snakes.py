8cc66d9aefcc506fd8945cab6b2797106b6a1389
h-r0rsch4ch <70857940+h-r0rsch4ch@users.noreply.github.com>
2021-04-14 11:58:30 -0500

    Update Great Snakes.py

--- Introduccion/Great Snakes.py
+++ Introduccion/Great Snakes.py
@@ -8,5 +8,5 @@ if sys.version_info.major == 2:
 
 ords = [81, 64, 75, 66, 70, 93, 73, 72, 1, 92, 109, 2, 84, 109, 66, 75, 70, 90, 2, 92, 79]
 
-print("Aqui esta tu flag, nene:")
+print("Toma tu flag, nene:")
 print("".join(chr(o ^ 0x32) for o in ords))
