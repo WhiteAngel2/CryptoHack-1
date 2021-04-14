35bcf906af10c027b3d26bf5d0d42516d8f9f4c8
h-r0rsch4ch <70857940+h-r0rsch4ch@users.noreply.github.com>
2021-04-14 15:23:41 -0500

    Update ASCII.py

--- General/ASCII.py
+++ General/ASCII.py
@@ -1,4 +1,4 @@
 ords = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
 
 print("Toma tu Flag, nene:")
-print("".join(chr(o) for o in ords))
+print("".join(chr(o) for o in ords)) # Convert decimals to ASCII code / Convierte los decimales a código ASCII
